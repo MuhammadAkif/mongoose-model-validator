85431a02644fb4cf24a1727c9a69cc2471afb78e
Muhammad Akif <akifmalhi786@gmail.com>
2019-02-20 16:57:39 +0500

    Adding unit tests and functionality for validation plugin.

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+class ValidateModel {
+    constructor() {
+        this.hasError = false;
+        this.type = null;
+        this.errors = [];
+    };
+
+    /**
+     * @param {Object}              [modelInsatnce={}(document object)]
+     *
+     * @returns {Promise}
+     */
+
+    static validateDocument() {
+        const validator = new ValidateModel();
+        return new Promise((resolve, reject) => {
+            this.validate((err) => {
+                if (err) {
+                    validator.hasError = true;
+                    if (err.name === 'ValidationError') {
+                        validator.type = 'ValidationError';
+                        for (let field in err.errors) {
+                            validator.errors.push(err.errors[field].message);
+                        }
+                        reject(validator);
+                    }
+                }
+                return resolve(validator);
+            });
+        });
+    }
+
+}
+
+
+module.exports = function (schema) {
+    schema.methods.validateDocument = ValidateModel.validateDocument;
+};
+
+// module.exports = {
+//     validateDocument: ValidateModel.ValidateDocument
+// };
---
+++ tests/index.js
@@ -0,0 +1,80 @@
+'use strict';
+
+let mongoose = require('mongoose');
+let expect = require('chai').expect;
+let mongooseModelValidator = require('../index');
+
+let BookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        minlength: [5, 'Title must be 10 characters long'],
+        require:[true, 'Please provide a title']
+    },
+    authorName: {
+        type: String,
+        minlength: [2, 'Author name must be 2 characters long'],
+        maxlength: [100, 'Author name should be less than 100 characters'],
+        require:[true, 'Please provide a title']
+    },
+    price: {
+        type: Number,
+        min: 10,
+        max: 1000000
+    }
+});
+
+BookSchema.plugin(mongooseModelValidator);
+
+let Book = mongoose.model('Book', BookSchema);
+
+describe('mongoose-model-validation', function() {
+    it('should validate the model', function () {
+        let book = new Book({
+            title: "Beauty and the Beast",
+            authorName: "Gabrielle-Suzanne de Villeneuve",
+            price: 1000
+        });
+        let promise = book.validateDocument();
+        promise.then((result) => {
+            expect(result.hasError).to.equal(false);
+            expect(result.type).to.be.null;
+            expect(result.errors).to.have.length(0);
+
+        }).catch((error) => {
+            console.error(error);
+        });
+    });
+
+    it('should return validation error for title and price', function() {
+        let book = new Book({
+            title: "Bea",
+            authorName: "Gabrielle-Suzanne de Villeneuve",
+            price: -1
+        });
+        let promise = book.validateDocument();
+        promise.then().catch((error) => {
+            expect(error.hasError).to.equal(true);
+            expect(error.type).to.equal("ValidationError");
+            expect(error.errors).to.have.length(2);
+        }).catch((error) => {
+            console.error(error);
+        });
+    });
+
+    it('should return validation error for Author Name', function(done) {
+        let book = new Book({
+            title: "Beauty and the Beast",
+            authorName: "D",
+            price: 100
+        });
+        let promise = book.validateDocument();
+        promise.then().catch((error) => {
+            expect(error.hasError).to.equal(true);
+            expect(error.type).to.equal("ValidationError");
+            expect(error.errors).to.have.length(1);
+            done();
+        }).catch((error) => {
+            console.error(error);
+        });
+    });
+});
